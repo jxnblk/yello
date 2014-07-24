daf981f54696da13a1462110ed3333a8af17b9ad
Brent Jackson <jxnblk@github.com>
2014-07-24 12:22:24 -0400

    Angular bootstrapping

--- js/app.js
+++ js/app.js
@@ -2,18 +2,24 @@ console.log('app');
 
 var app = angular.module('app', ['plangular']);
 
+app.controller('ApplicationCtrl', ['$scope', function($scope) {
+  console.log('ApplicationCtrl');
+  $scope.herro = 'application';
+}]);
+
 app.controller('HomeCtrl', ['$scope', function($scope) {
   console.log('HomeCtrl');
   $scope.test = 'herro';
 }]);
  
-//angular.element(document).on('ready', function() {
-
-//angular.element(document).ready(function() {
-//  angular.bootstrap(document, ['app']);
-//});
+angular.element(document).on('ready', function() {
+  console.log('ready');
+  angular.bootstrap(document, ['app']);
+  //angular.bootstrap(document.body, ['app']);
+});
 
 angular.element(document).on('page:load', function(){
   console.log('page load');
+  angular.bootstrap(document.body, ['app']);
 });
 
